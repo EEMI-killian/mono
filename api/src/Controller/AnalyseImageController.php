d188d292f0a8f8061f81917f860f3196b007d7e8
Killian ANGELY <killian.angely@outlook.fr>
2024-11-15 11:23:00 +0100

    (feat) add select champ

--- api/src/Controller/AnalyseImageController.php
+++ api/src/Controller/AnalyseImageController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Application\AnalyseImageUseCase;
+use App\AnalyseImage\AnalyseImageUseCase;
 use App\Gateways\Openai\OpenaiGateway;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -16,12 +16,12 @@ class AnalyseImageController
     {
         $data = json_decode(($request->getContent()), true);
 
-        // if (!isset($data['url']) || !isset($data['prompt']) || !isset($data['type'])) {
-        //     return new Response('Invalid input data', Response::HTTP_BAD_REQUEST);
-        // }
+        if (!isset($data['url']) || !isset($data['prompt']) || !isset($data['type'])) {
+            return new Response('Invalid input data', Response::HTTP_BAD_REQUEST);
+        }
         
         $openaiGateway = new OpenaiGateway($_ENV['OPENAI_API_KEY']);
-        $analyseImageUseCase = new AnalyseImageUseCase();
+        $analyseImageUseCase = new AnalyseImageUseCase($openaiGateway);
         $response = $analyseImageUseCase->execute($data);
         if (json_decode($response, true)['status'] === false) {
             return new Response(json_decode($response, true)['error'], Response::HTTP_INTERNAL_SERVER_ERROR);
