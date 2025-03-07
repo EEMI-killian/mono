d65fd3b59df72785bd484b6ff25167cfe9283c84
Alexmz1 <menez.alexis19@gmail.com>
2025-03-07 06:44:08 +0100

    Delete commentary migration

--- app/migrations/Version20250304233904.php
+++ app/migrations/Version20250304233904.php
@@ -19,14 +19,12 @@ final class Version20250304233904 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE item ADD is_public BOOLEAN DEFAULT FALSE NOT NULL');
         $this->addSql('ALTER TABLE outfit ADD is_public BOOLEAN DEFAULT FALSE NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE outfit DROP is_public');
         $this->addSql('ALTER TABLE item DROP is_public');
